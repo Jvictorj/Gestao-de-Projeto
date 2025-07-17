23718b417fc561fbe08fcf31fe1d0971ea2252b6
Jvictorj <jvictor.gomes@outlook.com>
2025-07-17 14:50:23 -0300

    feat: colocar lombok nas classes para abstração

--- src/main/java/dev/java10x/GestaoDeProjetos/Consultor/ConsultorModel.java
+++ src/main/java/dev/java10x/GestaoDeProjetos/Consultor/ConsultorModel.java
@@ -2,9 +2,15 @@ package dev.java10x.GestaoDeProjetos.Consultor;
 
 import dev.java10x.GestaoDeProjetos.Projetos.ProjetosModel;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Table (name = "tb_consultor")
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
 
 public class ConsultorModel {
 
@@ -21,47 +27,5 @@ public class ConsultorModel {
     @ManyToOne
     @JoinColumn(name = "projetos_id") //chave estrangeira
     private ProjetosModel projetos;
-
-    public ConsultorModel() { }
-
-    public ConsultorModel(String nome, String email, int idade, CargoDoConsultor cargo) {
-        this.nome = nome;
-        this.email = email;
-        this.idade = idade;
-        this.cargo = cargo;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public int getIdade () {
-        return idade;
-    }
-
-    public void setIdade () {
-        this.idade = idade;
-    }
-
-    public CargoDoConsultor getCargo () {
-        return cargo;
-    }
-
-    public void setCargo (CargoDoConsultor cargo) {
-        this.cargo = cargo;
-    }
-
 
 }
--- src/main/java/dev/java10x/GestaoDeProjetos/Projetos/ProjetosModel.java
+++ src/main/java/dev/java10x/GestaoDeProjetos/Projetos/ProjetosModel.java
@@ -2,12 +2,18 @@ package dev.java10x.GestaoDeProjetos.Projetos;
 
 import dev.java10x.GestaoDeProjetos.Consultor.ConsultorModel;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import java.time.LocalDate;
 import java.util.List;
 
 @Entity
 @Table(name = "tb_projeto")
-
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
 public class ProjetosModel {
 
     @Id
@@ -23,55 +29,5 @@ public class ProjetosModel {
     // Um projeto pode ter varios consultores
     @OneToMany(mappedBy = "projetos")
     private List<ConsultorModel> consultores;
-
-    public ProjetosModel() { }
-
-    public ProjetosModel(String titulo, String descricao, LocalDate dataInicio, LocalDate dataFimPrevista, StatusDoProjeto status) {
-        this.titulo = titulo;
-        this.descricao = descricao;
-        this.dataInicio = dataInicio;
-        this.dataFimPrevista = dataFimPrevista;
-        this.status = status;
-    }
-
-    public String getTitulo () {
-        return titulo;
-    }
-
-    public void setTitulo (String titulo) {
-        this.titulo = titulo;
-    }
-
-    public String getDescricao () {
-        return descricao;
-    }
-
-    public void setDescricao (String descricao) {
-        this.descricao = descricao;
-    }
-
-    public LocalDate getDataInicio () {
-        return dataInicio;
-    }
-
-    public void setDataInicio (LocalDate dataInicio) {
-        this.dataInicio = dataInicio;
-    }
-
-    public LocalDate getDataFimPrevista () {
-        return dataFimPrevista;
-    }
-
-    public void setDataFimPrevista (LocalDate dataFimPrevista) {
-        this.dataFimPrevista = dataFimPrevista;
-    }
-
-    public StatusDoProjeto getStatus () {
-        return status;
-    }
-
-    public void setStatus (StatusDoProjeto status) {
-        this.status = status;
-    }
 
 }
