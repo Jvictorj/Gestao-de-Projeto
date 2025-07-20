55cd74fb7011313e9b6caf1f0fbd52efbd28180c
Jvictorj <jvictor.gomes@outlook.com>
2025-07-20 08:43:17 -0300

    fix: Corrige erro de sintaxe na migration da tb_consultor
    A migration 'V2__Add_frase_tb_consultor.sql' estava falhando em
    ambientes que utilizam o banco de dados H2.
    
    O erro foi causado por um caractere de separador incorreto no arquivo.
    Onde deveria ser um hífen ('-'), havia sido colocado dois pontos (':').
    Este commit corrige a sintaxe para o formato válido.

--- src/main/resources/db/migrations/V2__Add_frase_tb_consultor.sql
+++ src/main/resources/db/migrations/V2__Add_frase_tb_consultor.sql
@@ -1,4 +1,4 @@
--- V2: Migrations para adicionar coluna de frase na tabela de consultor
+-- V2 - Migrations para adicionar coluna de frase na tabela de consultor
 
 ALTER TABLE tb_consultor
 ADD COLUMN frase VARCHAR(255)
